cecf5a28a40bc74c54b72522d7f8d130cdb4fb93
AdamGan <adamgan0527@gmail.com>
2016-10-12 21:02:01 +0800

    change region, get converted currency and option of max results

--- src/main/EbayCrawler.java
+++ src/main/EbayCrawler.java
@@ -42,14 +42,13 @@ public class EbayCrawler {
             + "&paginationInput.entriesPerPage={maxresults}";
     public static final String SERVICE_VERSION = "1.0.0";
     public static final String OPERATION_NAME = "findItemsByKeywords";
-    public static final String GLOBAL_ID = "EBAY-US";
+    public static final String GLOBAL_ID = "EBAY-SG";
     public final static int REQUEST_DELAY = 3000;
     public final static int MAX_RESULTS = 10;
     private int maxResults;
 
     public EbayCrawler() {
         this.maxResults = MAX_RESULTS;
-
     }
 
     public EbayCrawler(int maxResults) {
@@ -105,91 +104,110 @@ public class EbayCrawler {
         if (!ackToken.equals("Success")) {
             throw new Exception(" service returned an error");
         }
+        
+        XPathExpression resultExpression = xpath.compile("//findItemsByKeywordsResponse/searchResult");
+        NodeList tmpCountList = (NodeList) resultExpression.evaluate(doc, XPathConstants.NODESET);
+        Node tmpCount = tmpCountList.item(0);
+        //Node tmpCount = (Node) resultExpression;
+        //Node tmpCount = (Node) resultExpression.evaluate(doc, XPathConstants.STRING);
+        String resultCount =  (String) xpath.evaluate("@count", tmpCount, XPathConstants.STRING);
+        print("Item count: ", resultCount);
+        
+        if(Integer.parseInt(resultCount) == 0) {
+        	//no results
+        }
+        else {
+        	NodeList nodes = (NodeList) itemExpression.evaluate(doc, XPathConstants.NODESET);
 
-        NodeList nodes = (NodeList) itemExpression.evaluate(doc, XPathConstants.NODESET);
+            for (int i = 0; i < nodes.getLength(); i++) {
 
-        for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
 
-            Node node = nodes.item(i);
+                String itemId = (String) xpath.evaluate("itemId", node, XPathConstants.STRING);
+                String title = (String) xpath.evaluate("title", node, XPathConstants.STRING);
+                String itemUrl = (String) xpath.evaluate("viewItemURL", node, XPathConstants.STRING);
+                String galleryUrl = (String) xpath.evaluate("galleryURL", node, XPathConstants.STRING);
+                
+                String categoryId = (String) xpath.evaluate("primaryCategory/categoryId", node, XPathConstants.STRING);
+            	String categoryName = (String) xpath.evaluate("primaryCategory/categoryName", node, XPathConstants.STRING);
+                /*
+                NodeList tempCategory = (NodeList) xpath.evaluate("primaryCategory", node, XPathConstants.NODESET);
+                
+                for (int j = 0; j < tempCategory.getLength(); j++) {
+                	Node tmpNode = tempCategory.item(j);
+                	
+                	String categoryId = (String) xpath.evaluate("categoryId", tmpNode, XPathConstants.STRING);
+                	String categoryName = (String) xpath.evaluate("categoryName", tmpNode, XPathConstants.STRING);
+                }
+                */
+                String paymentMethod = (String) xpath.evaluate("paymentMethod", node, XPathConstants.STRING);
+                boolean autoPay = resolveBoolean((String) xpath.evaluate("autoPay", node, XPathConstants.STRING));
+                String postalCode = (String) xpath.evaluate("postalCode", node, XPathConstants.STRING);
+                String location = (String) xpath.evaluate("location", node, XPathConstants.STRING);
+                String country = (String) xpath.evaluate("country", node, XPathConstants.STRING);
+                
+                String shippingServiceCost  = (String) xpath.evaluate("shippingInfo/shippingServiceCost", node, XPathConstants.STRING);
+                String shippingCurrencyId = (String) xpath.evaluate("shippingInfo/shippingServiceCost/@currencyId", node, XPathConstants.STRING);
+                String shippingType = (String) xpath.evaluate("shippingInfo/shippingType", node, XPathConstants.STRING);
+                String shipToLocations = (String) xpath.evaluate("shippingInfo/shipToLocations", node, XPathConstants.STRING);
+                boolean expeditedShipping = resolveBoolean((String) xpath.evaluate("shippingInfo/expeditedShipping", node, XPathConstants.STRING));
+                boolean oneDayShippingAvailable = resolveBoolean((String) xpath.evaluate("shippingInfo/oneDayShippingAvailable", node, XPathConstants.STRING));
+                String handlingTime = (String) xpath.evaluate("shippingInfo/handlingTime", node, XPathConstants.STRING);
+                
+                /*
+                NodeList tempShipping = (NodeList) xpath.evaluate("shippingInfo", node, XPathConstants.NODESET);
+                
+                for (int j = 0; j < tempShipping.getLength(); j++) {
+                	Node tmpNode = tempShipping.item(j);
+                	
+                	String shippingServiceCost  = (String) xpath.evaluate("shippingServiceCost", node, XPathConstants.STRING);
+                    String currencyId = (String) xpath.evaluate("currencyId", node, XPathConstants.STRING);
+                    String shippingType = (String) xpath.evaluate("shippingType", node, XPathConstants.STRING);
+                    String shipToLocations = (String) xpath.evaluate("shipToLocations", node, XPathConstants.STRING);
+                    boolean expeditedShipping = resolveBoolean((String) xpath.evaluate("expeditedShipping", node, XPathConstants.STRING));
+                    boolean oneDayShippingAvailable = resolveBoolean((String) xpath.evaluate("oneDayShippingAvailable", node, XPathConstants.STRING));
+                    String handlingTime = (String) xpath.evaluate("handlingTime", node, XPathConstants.STRING);
+                }
+                */
+                String currentPrice = (String) xpath.evaluate("sellingStatus/currentPrice", node, XPathConstants.STRING);
+                String sellingCurrencyId = (String) xpath.evaluate("sellingStatus/currentPrice/@currencyId", node, XPathConstants.STRING);
+                
+                String convertedCurrentPrice = (String) xpath.evaluate("sellingStatus/convertedCurrentPrice", node, XPathConstants.STRING);
+                String sellingConvertedCurrencyId = (String) xpath.evaluate("sellingStatus/convertedCurrentPrice/@currencyId", node, XPathConstants.STRING);
+                
+                String conditionDisplayName = (String) xpath.evaluate("conditionDisplayName", node, XPathConstants.STRING);
 
-            String itemId = (String) xpath.evaluate("itemId", node, XPathConstants.STRING);
-            String title = (String) xpath.evaluate("title", node, XPathConstants.STRING);
-            String itemUrl = (String) xpath.evaluate("viewItemURL", node, XPathConstants.STRING);
-            String galleryUrl = (String) xpath.evaluate("galleryURL", node, XPathConstants.STRING);
-            
-            String categoryId = (String) xpath.evaluate("primaryCategory/categoryId", node, XPathConstants.STRING);
-        	String categoryName = (String) xpath.evaluate("primaryCategory/categoryName", node, XPathConstants.STRING);
-            /*
-            NodeList tempCategory = (NodeList) xpath.evaluate("primaryCategory", node, XPathConstants.NODESET);
-            
-            for (int j = 0; j < tempCategory.getLength(); j++) {
-            	Node tmpNode = tempCategory.item(j);
-            	
-            	String categoryId = (String) xpath.evaluate("categoryId", tmpNode, XPathConstants.STRING);
-            	String categoryName = (String) xpath.evaluate("categoryName", tmpNode, XPathConstants.STRING);
+                print("currentPrice", currentPrice);
+                print("sellingCurrencyId", sellingCurrencyId);
+                print("convertedCurrentPrice", convertedCurrentPrice);
+                print("sellingConvertedCurrencyId", sellingConvertedCurrencyId);
+                
+                print("itemId", itemId);
+                print("title", title);
+                print("galleryUrl", galleryUrl);
+                print("categoryId", categoryId);
+                print("categoryName", categoryName);
+                print("paymentMethod", paymentMethod);
+                print("autoPay", autoPay);
+                print("postalCode", postalCode);
+                print("location", location);
+                print("country", country);
+                
+                print("shippingServiceCost", shippingServiceCost);
+                print("shippingCurrencyId", shippingCurrencyId);
+                print("shippingType", shippingType);
+                print("shipToLocations", shipToLocations);
+                
+                print("expeditedShipping", expeditedShipping);
+                print("oneDayShippingAvailable", oneDayShippingAvailable);
+                print("handlingTime", handlingTime);
+                print("conditionDisplayName", conditionDisplayName);
+                
+                System.out.println();
+                System.out.println();
+
             }
-            */
-            String paymentMethod = (String) xpath.evaluate("paymentMethod", node, XPathConstants.STRING);
-            boolean autoPay = resolveBoolean((String) xpath.evaluate("autoPay", node, XPathConstants.STRING));
-            String postalCode = (String) xpath.evaluate("postalCode", node, XPathConstants.STRING);
-            String location = (String) xpath.evaluate("location", node, XPathConstants.STRING);
-            String country = (String) xpath.evaluate("country", node, XPathConstants.STRING);
-            
-            String shippingServiceCost  = (String) xpath.evaluate("shippingInfo/shippingServiceCost", node, XPathConstants.STRING);
-            String shippingCurrencyId = (String) xpath.evaluate("shippingInfo/shippingServiceCost/@currencyId", node, XPathConstants.STRING);
-            String shippingType = (String) xpath.evaluate("shippingInfo/shippingType", node, XPathConstants.STRING);
-            String shipToLocations = (String) xpath.evaluate("shippingInfo/shipToLocations", node, XPathConstants.STRING);
-            boolean expeditedShipping = resolveBoolean((String) xpath.evaluate("shippingInfo/expeditedShipping", node, XPathConstants.STRING));
-            boolean oneDayShippingAvailable = resolveBoolean((String) xpath.evaluate("shippingInfo/oneDayShippingAvailable", node, XPathConstants.STRING));
-            String handlingTime = (String) xpath.evaluate("shippingInfo/handlingTime", node, XPathConstants.STRING);
-            
-            /*
-            NodeList tempShipping = (NodeList) xpath.evaluate("shippingInfo", node, XPathConstants.NODESET);
-            
-            for (int j = 0; j < tempShipping.getLength(); j++) {
-            	Node tmpNode = tempShipping.item(j);
-            	
-            	String shippingServiceCost  = (String) xpath.evaluate("shippingServiceCost", node, XPathConstants.STRING);
-                String currencyId = (String) xpath.evaluate("currencyId", node, XPathConstants.STRING);
-                String shippingType = (String) xpath.evaluate("shippingType", node, XPathConstants.STRING);
-                String shipToLocations = (String) xpath.evaluate("shipToLocations", node, XPathConstants.STRING);
-                boolean expeditedShipping = resolveBoolean((String) xpath.evaluate("expeditedShipping", node, XPathConstants.STRING));
-                boolean oneDayShippingAvailable = resolveBoolean((String) xpath.evaluate("oneDayShippingAvailable", node, XPathConstants.STRING));
-                String handlingTime = (String) xpath.evaluate("handlingTime", node, XPathConstants.STRING);
-            }
-            */
-            String currentPrice = (String) xpath.evaluate("sellingStatus/currentPrice", node, XPathConstants.STRING);
-            String sellingCurrencyId = (String) xpath.evaluate("sellingStatus/currentPrice/@currencyId", node, XPathConstants.STRING);
-
-            String conditionDisplayName = (String) xpath.evaluate("conditionDisplayName", node, XPathConstants.STRING);
-
-            print("currentPrice", currentPrice);
-            print("sellingCurrencyId", sellingCurrencyId);
-            
-            print("itemId", itemId);
-            print("title", title);
-            print("galleryUrl", galleryUrl);
-            print("categoryId", categoryId);
-            print("categoryName", categoryName);
-            print("paymentMethod", paymentMethod);
-            print("autoPay", autoPay);
-            print("postalCode", postalCode);
-            print("location", location);
-            print("country", country);
-            
-            print("shippingServiceCost", shippingServiceCost);
-            print("shippingCurrencyId", shippingCurrencyId);
-            print("shippingType", shippingType);
-            print("shipToLocations", shipToLocations);
-            
-            print("expeditedShipping", expeditedShipping);
-            print("oneDayShippingAvailable", oneDayShippingAvailable);
-            print("handlingTime", handlingTime);
-            print("conditionDisplayName", conditionDisplayName);
-            
-            System.out.println();
-            System.out.println();
-
+        	
         }
 
         is.close();
@@ -226,15 +244,15 @@ public class EbayCrawler {
 
   public static void main(String[] args) throws Exception {
   	
-	  EbayCrawler driver = new EbayCrawler();
-      String tag = "Velo binding machine";
+	  EbayCrawler driver = new EbayCrawler();	//default 10 results
+      String tag = "cd";
       driver.run(java.net.URLEncoder.encode(tag, "UTF-8"));
       
   }
   
-  public void findByName(String query) throws Exception {
+  public void findByName(String query, int numberOfMaxResults) throws Exception {
 	  	
-	  EbayCrawler driver = new EbayCrawler();
+	  EbayCrawler driver = new EbayCrawler(numberOfMaxResults);
       driver.run(java.net.URLEncoder.encode(query, "UTF-8"));
       
       //return type TODO
