ba303562f0f2a4e731bb8428205572d4b6fa9e95
AdamGan <adamgan0527@gmail.com>
2016-10-12 20:32:39 +0800

    working ebay skeleton

--- src/main/EbayCrawler.java
+++ src/main/EbayCrawler.java
@@ -4,17 +4,244 @@ package main;
 import com.ebay.soap.eBLBaseComponents.*;
 import com.ebay.sdk.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import org.apache.axis.types.URI.MalformedURIException;
 import java.rmi.RemoteException;
 import java.util.Properties;
 import java.io.IOException;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 
 public class EbayCrawler {
+	
+	public final static String EBAY_APP_ID = "AdamGan-ECommerc-PRD-59f210a48-6b92885d";
+    public final static String EBAY_FINDING_SERVICE_URI = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME="
+            + "{operation}&SERVICE-VERSION={version}&SECURITY-APPNAME="
+            + "{applicationId}&GLOBAL-ID={globalId}&keywords={keywords}"
+            + "&paginationInput.entriesPerPage={maxresults}";
+    public static final String SERVICE_VERSION = "1.0.0";
+    public static final String OPERATION_NAME = "findItemsByKeywords";
+    public static final String GLOBAL_ID = "EBAY-US";
+    public final static int REQUEST_DELAY = 3000;
+    public final static int MAX_RESULTS = 10;
+    private int maxResults;
 
-  public static void main(String[] args) throws MalformedURIException, RemoteException {
+    public EbayCrawler() {
+        this.maxResults = MAX_RESULTS;
+
+    }
+
+    public EbayCrawler(int maxResults) {
+        this.maxResults = maxResults;
+    }
+
+    public void run(String tag) throws Exception {
+
+        String address = createAddress(tag);
+        print("sending request to :: ", address);
+        String response = ebayURLReader.read(address);
+        print("response :: ", response);
+        //process xml dump returned from EBAY
+        processResponse(response);
+        //Honor rate limits - wait between results
+        Thread.sleep(REQUEST_DELAY);
+
+
+    }
+
+    private String createAddress(String tag) {
+
+        //substitute token
+        String address = EbayCrawler.EBAY_FINDING_SERVICE_URI;
+        address = address.replace("{version}", EbayCrawler.SERVICE_VERSION);
+        address = address.replace("{operation}", EbayCrawler.OPERATION_NAME);
+        address = address.replace("{globalId}", EbayCrawler.GLOBAL_ID);
+        address = address.replace("{applicationId}", EbayCrawler.EBAY_APP_ID);
+        address = address.replace("{keywords}", tag);
+        address = address.replace("{maxresults}", "" + this.maxResults);
+
+        return address;
+
+    }
+
+    private void processResponse(String response) throws Exception {
+
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        InputStream is = new ByteArrayInputStream(response.getBytes("UTF-8"));
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = domFactory.newDocumentBuilder();
+
+
+        Document doc = builder.parse(is);
+        //OutputStream myOutput = new FileOutputStream("sample.txt");
+        //printDocument(doc, myOutput);
+        XPathExpression ackExpression = xpath.compile("//findItemsByKeywordsResponse/ack");
+        XPathExpression itemExpression = xpath.compile("//findItemsByKeywordsResponse/searchResult/item");
+
+        String ackToken = (String) ackExpression.evaluate(doc, XPathConstants.STRING);
+        print("ACK from ebay API :: ", ackToken);
+        if (!ackToken.equals("Success")) {
+            throw new Exception(" service returned an error");
+        }
+
+        NodeList nodes = (NodeList) itemExpression.evaluate(doc, XPathConstants.NODESET);
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+
+            Node node = nodes.item(i);
+
+            String itemId = (String) xpath.evaluate("itemId", node, XPathConstants.STRING);
+            String title = (String) xpath.evaluate("title", node, XPathConstants.STRING);
+            String itemUrl = (String) xpath.evaluate("viewItemURL", node, XPathConstants.STRING);
+            String galleryUrl = (String) xpath.evaluate("galleryURL", node, XPathConstants.STRING);
+            
+            String categoryId = (String) xpath.evaluate("primaryCategory/categoryId", node, XPathConstants.STRING);
+        	String categoryName = (String) xpath.evaluate("primaryCategory/categoryName", node, XPathConstants.STRING);
+            /*
+            NodeList tempCategory = (NodeList) xpath.evaluate("primaryCategory", node, XPathConstants.NODESET);
+            
+            for (int j = 0; j < tempCategory.getLength(); j++) {
+            	Node tmpNode = tempCategory.item(j);
+            	
+            	String categoryId = (String) xpath.evaluate("categoryId", tmpNode, XPathConstants.STRING);
+            	String categoryName = (String) xpath.evaluate("categoryName", tmpNode, XPathConstants.STRING);
+            }
+            */
+            String paymentMethod = (String) xpath.evaluate("paymentMethod", node, XPathConstants.STRING);
+            boolean autoPay = resolveBoolean((String) xpath.evaluate("autoPay", node, XPathConstants.STRING));
+            String postalCode = (String) xpath.evaluate("postalCode", node, XPathConstants.STRING);
+            String location = (String) xpath.evaluate("location", node, XPathConstants.STRING);
+            String country = (String) xpath.evaluate("country", node, XPathConstants.STRING);
+            
+            String shippingServiceCost  = (String) xpath.evaluate("shippingInfo/shippingServiceCost", node, XPathConstants.STRING);
+            String shippingCurrencyId = (String) xpath.evaluate("shippingInfo/shippingServiceCost/@currencyId", node, XPathConstants.STRING);
+            String shippingType = (String) xpath.evaluate("shippingInfo/shippingType", node, XPathConstants.STRING);
+            String shipToLocations = (String) xpath.evaluate("shippingInfo/shipToLocations", node, XPathConstants.STRING);
+            boolean expeditedShipping = resolveBoolean((String) xpath.evaluate("shippingInfo/expeditedShipping", node, XPathConstants.STRING));
+            boolean oneDayShippingAvailable = resolveBoolean((String) xpath.evaluate("shippingInfo/oneDayShippingAvailable", node, XPathConstants.STRING));
+            String handlingTime = (String) xpath.evaluate("shippingInfo/handlingTime", node, XPathConstants.STRING);
+            
+            /*
+            NodeList tempShipping = (NodeList) xpath.evaluate("shippingInfo", node, XPathConstants.NODESET);
+            
+            for (int j = 0; j < tempShipping.getLength(); j++) {
+            	Node tmpNode = tempShipping.item(j);
+            	
+            	String shippingServiceCost  = (String) xpath.evaluate("shippingServiceCost", node, XPathConstants.STRING);
+                String currencyId = (String) xpath.evaluate("currencyId", node, XPathConstants.STRING);
+                String shippingType = (String) xpath.evaluate("shippingType", node, XPathConstants.STRING);
+                String shipToLocations = (String) xpath.evaluate("shipToLocations", node, XPathConstants.STRING);
+                boolean expeditedShipping = resolveBoolean((String) xpath.evaluate("expeditedShipping", node, XPathConstants.STRING));
+                boolean oneDayShippingAvailable = resolveBoolean((String) xpath.evaluate("oneDayShippingAvailable", node, XPathConstants.STRING));
+                String handlingTime = (String) xpath.evaluate("handlingTime", node, XPathConstants.STRING);
+            }
+            */
+            String currentPrice = (String) xpath.evaluate("sellingStatus/currentPrice", node, XPathConstants.STRING);
+            String sellingCurrencyId = (String) xpath.evaluate("sellingStatus/currentPrice/@currencyId", node, XPathConstants.STRING);
+
+            String conditionDisplayName = (String) xpath.evaluate("conditionDisplayName", node, XPathConstants.STRING);
+
+            print("currentPrice", currentPrice);
+            print("sellingCurrencyId", sellingCurrencyId);
+            
+            print("itemId", itemId);
+            print("title", title);
+            print("galleryUrl", galleryUrl);
+            print("categoryId", categoryId);
+            print("categoryName", categoryName);
+            print("paymentMethod", paymentMethod);
+            print("autoPay", autoPay);
+            print("postalCode", postalCode);
+            print("location", location);
+            print("country", country);
+            
+            print("shippingServiceCost", shippingServiceCost);
+            print("shippingCurrencyId", shippingCurrencyId);
+            print("shippingType", shippingType);
+            print("shipToLocations", shipToLocations);
+            
+            print("expeditedShipping", expeditedShipping);
+            print("oneDayShippingAvailable", oneDayShippingAvailable);
+            print("handlingTime", handlingTime);
+            print("conditionDisplayName", conditionDisplayName);
+            
+            System.out.println();
+            System.out.println();
+
+        }
+
+        is.close();
+
+    }
+    
+    private boolean resolveBoolean(String bool) {
+    	if (bool.equalsIgnoreCase("true")) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    private void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        transformer.transform(new DOMSource(doc), 
+             new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+    }
+
+    private void print(String name, String value) {
+        System.out.println(name + "::" + value);
+    }
+    
+    private void print(String name, boolean value) {
+        System.out.println(name + "::" + value);
+    }
+
+  public static void main(String[] args) throws Exception {
   	
+	  EbayCrawler driver = new EbayCrawler();
+      String tag = "Velo binding machine";
+      driver.run(java.net.URLEncoder.encode(tag, "UTF-8"));
+      
+  }
+  
+  public void findByName(String query) throws Exception {
+	  	
+	  EbayCrawler driver = new EbayCrawler();
+      driver.run(java.net.URLEncoder.encode(query, "UTF-8"));
+      
+      //return type TODO
+      
+  }
+
+  /*
 //  Read properties file to load developer credentials
     Properties keys = new Properties();
     //String root = System.getProperty("user.dir");
@@ -41,6 +268,7 @@ public class EbayCrawler {
     context.setApiCredential(credential);
   
     // set eBay server URL to call
+    //context.setApiServerUrl( "https://api.sandbox.ebay.com/wsapi" );  // sandbox
     context.setApiServerUrl( "http://svcs.ebay.com/services/search/FindingService/v1" );  // production
     
     // set timeout in milliseconds - 3 minutes
@@ -59,14 +287,25 @@ public class EbayCrawler {
     
     // create ApiCall object - we'll use it to make the call
     ApiCall call = new ApiCall( context );
-    /*
+    
     //example to get time
     // create soap api request and response objects
     GeteBayOfficialTimeRequestType request = new GeteBayOfficialTimeRequestType();
     AbstractResponseType response;
     
-    // make the call and handle the response
+    //com.ebay.soap.eBLBaseComponents.GetContextualKeywordsRequestType;
+    */
+    /* public final static String EBAY_FINDING_SERVICE_URI = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME="
+            + "{operation}&SERVICE-VERSION={version}&SECURITY-APPNAME="
+            + "{applicationId}&GLOBAL-ID={globalId}&keywords={keywords}"
+            + "&paginationInput.entriesPerPage={maxresults}";
+     
     
+     */
+
+    
+    // make the call and handle the response
+    /*
     try {
     	//api calls
     	
@@ -76,6 +315,19 @@ public class EbayCrawler {
         // Result inherits from AbstractResponseType
         Calendar cal = response.getTimestamp();            // ebay time is returned in gmt 
 		
+        // Display official ebay time in gmt
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;   // java months go from 0-11; make human readable
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int hour24 = cal.get(Calendar.HOUR_OF_DAY);
+        int min = cal.get(Calendar.MINUTE);
+        int sec = cal.get(Calendar.SECOND);
+        System.out.println("official ebay time in gmt is: " + year + "-" + month + "-" + day + " " + hour24 + ":" + min + ":" + sec);
+        
+        // Convert ebay time to local time and display
+        SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss Z");
+        System.out.println("in your time zone " + formatter.format( cal.getTime() ));
 
         
     } catch (ApiException ae) {
@@ -85,7 +337,8 @@ public class EbayCrawler {
     } catch (SdkException se) {
     	System.out.println(se);
     }
-    */		
+    	
   }
+  */
  }
 
---
+++ src/main/ebayURLReader.java
@@ -0,0 +1,33 @@
+package main;
+
+//from stackoverflow: rjha94
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ebayURLReader {
+
+    public static String read(String address) throws Exception{
+
+        URL url = new URL(address);
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+
+        String line;
+        String response;
+        long totalBytes = 0  ;
+
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+            totalBytes += line.getBytes("UTF-8").length ;
+        }
+
+        response = builder.toString();
+
+        return response ;
+    }
+
+}
