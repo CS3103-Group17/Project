dd17f29ca4bd84780ec5324042965fb64d1c7d97
RyanAlexanderH <ryan.alexander.hanalim@gmail.com>
2016-11-06 10:48:11 +0800

    Initialise fb crawler and model

---
+++ src/api/socials/FacebookCrawler.java
@@ -0,0 +1,21 @@
+package api.socials;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import model.socials.FbData;
+
+public class FacebookCrawler {
+	
+	private final static String APP_ID = "721632677991360";
+	private final static String APP_SECRET = "35faeb0e2a2d72667974482cb366b718";
+	
+	public ArrayList<FbData> getFbPosts(String searchTerm) {
+		ArrayList<FbData> list = new ArrayList<FbData>();
+		
+		return list;
+	}
+	
+}
